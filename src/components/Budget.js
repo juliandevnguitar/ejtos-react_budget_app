6e92f1927ff65a673c6a60b13643abae908d8a9d
Julian Garcia <julian.music.003@gmail.com>
2023-09-04 17:03:48 -0400

    cambios 1 y 2

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,15 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    let { budget } = useContext(AppContext);
+   function newBudget(val) {
+       budget = val;
+   }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <label htmlFor="numero">Budget:</label>
+            <input type="number" id="numero" name="numero" min="0" max="20000" step="10" value={budget} onChange={(event) => newBudget(event.target.value)}/>
+
         </div>
     );
 };
